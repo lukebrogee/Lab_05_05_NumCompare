7b90defffb655242fb95291ea1e683ef8166f4ba
Luke Brogee <lukebrogee@gmail.com>
2022-09-25 21:00:20 -0400

    Initial commit

---
+++ src/NumCompare.java
@@ -0,0 +1,16 @@
+public class NumCompare {
+    public static void main(String[] args) {
+        System.out.println("Enter two numbers to compare");
+        double numberOne = 4.4534534;
+        double numberTwo = 8.4534534;
+
+        System.out.println("Are the numbers the same?");
+
+        if (numberOne != numberTwo){
+            System.out.println("The numbers are not the same");
+        } else {
+            System.out.println("The numbers are equal!");
+        }
+
+    }
+}
